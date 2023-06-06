13cb44b88553a4bb027983400e0101e00aa45927
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-05 22:56:04 -0400

    Creacion de rutas y paginas de navegacion

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -41,40 +41,16 @@ import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "las la-graduation-cap",
-    link: "https://quasar.dev",
+    title: "Typography",
+    caption: "Tipos de letra",
+    icon: "las la-font",
+    link: "typography",
   },
   {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "lab la-github",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "lab la-discord",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "lab la-forumbee",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "lab la-twitter",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "lab la-facebook",
-    link: "https://facebook.quasar.dev",
+    title: "Flex Layour",
+    caption: "Estilos de flex",
+    icon: "lab la-layer-group",
+    link: "flex",
   },
   {
     title: "Quasar Awesome",
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,28 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "typography",
+        name: "TypographyPage",
+        component: () => import("src/pages/TypographyPage.vue"),
+      },
+      {
+        path: "flex",
+        name: "FlexPage",
+        component: () => import("src/pages/FlexPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
